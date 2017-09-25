fa66e40da397dd585fbb1257d88f7c3e513d922a
Sami Konzo <samikonzo@gmail.com>
2017-09-25 10:44:20 +0400

    cubes: arrange elements

---
+++ script/cubes.js
@@ -0,0 +1,27 @@
+'use strict'
+var l = console.log;
+
+var cubesLis = document.querySelectorAll('#cubes > ul > li');
+
+/*
+
+setTimeout( function f(){
+	var count = Math.round(Math.random() * cubesLis.length);
+	var nums = [].map.call(cubesLis, (el, i) => {
+		return i;
+	})
+	var choosedNums = []
+	
+	for(var i = 0; i < count; i++){
+		var currentNum = Math.round(Math.random() * (nums.length - 1))
+		choosedNums.push(nums.splice(currentNum, 1)[0]);
+	}
+
+	choosedNums.forEach( num => {
+		cubesLis[num].classList.toggle('today');
+	})
+
+	setTimeout(f, 5000);
+}, 500)
+
+*/
