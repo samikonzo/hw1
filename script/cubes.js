f5eda233864a6322bf7bb414bc4424d58bd775b1
Sami Konzo <samikonzo@gmail.com>
2017-10-12 12:20:41 +0300

    :autosave

--- script/cubes.js
+++ script/cubes.js
@@ -32,13 +32,27 @@ cubes.forEach(cube => {
 	cube.icon = cube.querySelector('.cube__icon');
 	cube.info = cube.querySelector('.cube__info');
 })
+
 $('.cube').mouseover(function(e){
 	$('.cube__icon', this).addClass('cube__icon--size-small');
 	$('.cube__info', this).addClass('cube__info--position-week').removeClass('cube__info--position-today');
-
 })
+
 $('.cube').mouseout(function(e){
+	var cube = $(this)[0];
+
+	if(isParentContainsChild(cube, e.relatedTarget)){
+		return
+	}
+
 	$('.cube__icon', this).removeClass('cube__icon--size-small');
 	$('.cube__info', this).removeClass('cube__info--position-week').addClass('cube__info--position-today');
+
 })
+
+function isParentContainsChild(parent, child){
+	if(!parent || !child) return false
+	var result = parent.compareDocumentPosition(child) & 16;
+	return result
+}
 
