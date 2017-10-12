e596bcab23f802c121345d6b3dc9ffd1f5d6f30f
Sami Konzo <samikonzo@gmail.com>
2017-10-12 10:23:26 +0300

    BEM: done

--- script/cubes.js
+++ script/cubes.js
@@ -1,15 +1,13 @@
 'use strict'
 var l = console.log;
 
-var CHANGE_TIME = 2000;
+/*var CHANGE_TIME = 2000;
 
-var cubesLis = document.querySelectorAll('#cubes > ul > li');
-
-
+var cubes = document.querySelectorAll('.cube');
 
 setTimeout( function f(){
-	var count = Math.round(Math.random() * cubesLis.length);
-	var nums = [].map.call(cubesLis, (el, i) => {
+	var count = Math.round(Math.random() * cubes.length);
+	var nums = [].map.call(cubes, (el, i) => {
 		return i;
 	})
 	var choosedNums = []
@@ -20,9 +18,27 @@ setTimeout( function f(){
 	}
 
 	choosedNums.forEach( num => {
-		cubesLis[num].classList.toggle('today');
+		cubes[num].querySelector('.cube__icon').classList.toggle('cube__icon--size-small')
+		cubes[num].querySelector('.cube__info').classList.toggle('cube__info--position-week');
+		cubes[num].querySelector('.cube__info').classList.toggle('cube__info--position-today');
 	})
 
 	setTimeout(f, CHANGE_TIME);
 }, CHANGE_TIME)
+*/
+
+var cubes = document.querySelectorAll('.cube');
+cubes.forEach(cube => {
+	cube.icon = cube.querySelector('.cube__icon');
+	cube.info = cube.querySelector('.cube__info');
+})
+$('.cube').mouseover(function(e){
+	$('.cube__icon', this).addClass('cube__icon--size-small');
+	$('.cube__info', this).addClass('cube__info--position-week').removeClass('cube__info--position-today');
+
+})
+$('.cube').mouseout(function(e){
+	$('.cube__icon', this).removeClass('cube__icon--size-small');
+	$('.cube__info', this).removeClass('cube__info--position-week').addClass('cube__info--position-today');
+})
 
