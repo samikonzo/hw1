b60b08351fa4b04b7b1b7ed052b026007b9ee3e5
Sami Konzo <samikonzo@gmail.com>
2017-11-21 15:47:43 +0300

    flex try 1

--- script/cubes.js
+++ script/cubes.js
@@ -1,32 +1,6 @@
 'use strict'
 var l = console.log;
 
-/*var CHANGE_TIME = 2000;
-
-var cubes = document.querySelectorAll('.cube');
-
-setTimeout( function f(){
-	var count = Math.round(Math.random() * cubes.length);
-	var nums = [].map.call(cubes, (el, i) => {
-		return i;
-	})
-	var choosedNums = []
-	
-	for(var i = 0; i < count; i++){
-		var currentNum = Math.round(Math.random() * (nums.length - 1))
-		choosedNums.push(nums.splice(currentNum, 1)[0]);
-	}
-
-	choosedNums.forEach( num => {
-		cubes[num].querySelector('.cube__icon').classList.toggle('cube__icon--size-small')
-		cubes[num].querySelector('.cube__info').classList.toggle('cube__info--position-week');
-		cubes[num].querySelector('.cube__info').classList.toggle('cube__info--position-today');
-	})
-
-	setTimeout(f, CHANGE_TIME);
-}, CHANGE_TIME)
-*/
-
 var cubes = document.querySelectorAll('.cube');
 cubes.forEach(cube => {
 	cube.icon = cube.querySelector('.cube__icon');
